d776b581ba70fa8085ebdb4e8c899b082999e124
Lux Le <luclt399471@gmail.com>
2017-11-28 11:30:06 +0700

    Adding releate project

---
+++ TMSytems.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TMSytems.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
