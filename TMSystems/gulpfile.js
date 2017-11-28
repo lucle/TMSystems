65bc6d3b403b940e2d88ed1c997cbee56927ea6e
Lux Le <luclt399471@gmail.com>
2017-11-28 11:09:11 +0700

    Adding project

---
+++ TMSystems/gulpfile.js
@@ -0,0 +1,30 @@
+﻿// <binding AfterBuid='copy-modules' />
+"use strict";
+
+var gulp = require('gulp'),
+    clean = require('gulp-clean'),
+    glob = require('glob'),
+    rimraf = require('rimraf'),
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-cssmin'),
+    uglify = require('gulp-uglify'),
+    ignore = require('gulp-ignore'),
+    del = require('del');
+
+var paths = {
+    webroot:"./wwwroot/",
+    bower:".bower_components/"
+};
+
+var bower = {
+    "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,woff2,eot}",
+    "font-awesome": "components-font-awesome/**/*.{css,tff,svg,woff,woff2,eot,otf}",
+    "jquery": "jquery/dist/jquery*.{js,map}",
+    "jquery-validation": "jquery-validation/dist/*.js",
+    "jquery-validation-unobtrusive": "jquery-validation-unobtrusive/*.js"
+};
+
+gulp.task('clean-module', function () {
+    return gulp.src([])
+});
+
